a4ab11d3b0e87115a43b5f0c647fbeccf6a06389
yjhong89 <yjhong@snu.ac.kr>
2018-03-28 20:06:00 +0900

    Fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from train import train
 #import inference
 
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 def main():
